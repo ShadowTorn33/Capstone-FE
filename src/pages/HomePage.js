3bf8029e942bb2fafea55b7379f404cb492a1594
ShadowTorn33 <austincheer94@gmail.com>
2023-02-05 16:30:09 -0800

    css5

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,9 +4,7 @@ import Card from 'react-bootstrap/Card';
 const HomePage = () => {
   return (
     <div>
-    <Card className="bg-dark text-white">
-      <Card.Img src="https://i.imgur.com/aCcTyjq.jpg" alt="Card image" />
-    </Card>
+    
     </div>
   )
 }
